b32ff0a51b0c8920244ca43e5cabd1a24da7d5c4
unknown <gigs612@gmail.com>
2019-03-19 21:02:27 -0400

    add boundaries

--- CarRacing/main.py
+++ CarRacing/main.py
@@ -18,48 +18,51 @@ pygame.display.set_caption("Car Racing") #title of game window
 
 carImg = pygame.image.load("Images/car.png")
 
+clock = pygame.time.Clock() #timing for game
+
 def car(x,y):
     gameDisplay.blit(carImg, (x,y)) #draws the car(in this case, the image) at (x,y) position
 
-
-#setting the starting position of car
-x = 0.45 * display_width
-y = 0.8 * display_height 
-
-x_change = 0 #change in x direction(when key is pressed)
- 
-clock = pygame.time.Clock() #timing for game
-
-#the game is about not crashing into another object- so a crash variable is used to end the game
-isCrashed = False
-
 #game loop
-while not isCrashed:
-    #creating a list of events(in this case, key presses) that may happen
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:#in this event, the user decides to quit the game
-            isCrashed = True
-        
-        if event.type == pygame.KEYDOWN:
-            if event.key == pygame.K_LEFT:
-                x_change = -5
-            if event.key == pygame.K_RIGHT:
-                x_change = 5
+def game_loop():
+    #setting the starting position of car
+    x = 0.45 * display_width
+    y = 0.8 * display_height 
+
+    x_change = 0 #change in x direction(when key is pressed)
+    
+    #the game is about not crashing into another object- so a crash variable is used to end the game
+    isCrashed = False
+    gameExit = False
+    car_width = 128
+    while not gameExit:
+        #creating a list of events(in this case, key presses) that may happen
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:#in this event, the user decides to quit the game
+                gameExit = True
             
-        #when key is released, there must be no change in position
-        if event.type == pygame.KEYUP:
-            if event.key == pygame.K_LEFT or pygame.K_RIGHT:
-                x_change = 0
-    
-    x+=x_change
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_LEFT:
+                    x_change = -5
+                if event.key == pygame.K_RIGHT:
+                    x_change = 5
+                
+            #when key is released, there must be no change in position
+            if event.type == pygame.KEYUP:
+                if event.key == pygame.K_LEFT or pygame.K_RIGHT:
+                    x_change = 0
+        
+        x+=x_change
 
+        gameDisplay.fill(WHITE)#change background color
+        car(x,y)
 
-    
-    gameDisplay.fill(WHITE)#change background color
-    car(x,y)
+        if x > (display_width - car_width) or x < 0:
+            gameExit = True
 
-    pygame.display.update() #can update specific parameters, or entire display(no parameters)
-    clock.tick(60) #frames per second
+        pygame.display.update() #can update specific parameters, or entire display(no parameters)
+        clock.tick(60) #frames per second
 
+game_loop()
 pygame.quit()
 quit
